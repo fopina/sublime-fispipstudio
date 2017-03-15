063e636bc7ee0ce963e9ae79805bedf40f831492
Filipe Pina <filipe.pina@impactzero.pt>
2017-03-15 11:58:49 +0000

    working on #2
    syntax highlighting
    Complile and Link and Run PSL
    Command Palette

--- fispipstudio.py
+++ fispipstudio.py
@@ -2,7 +2,7 @@ import sublime
 import sublime_plugin
 import os
 import getpass
-from io import BytesIO
+from io import BytesIO, StringIO
 from fispip import PIP
 
 # TODO: useless for now, as package requires ST3
@@ -12,14 +12,15 @@ IS_ST2 = int(sublime.version()) < 3000
 class FisPipCommand(sublime_plugin.WindowCommand):
     def run(self, paths=None):
         c = self.find_config(paths)
-        s = Wrapper(c)
+        s = None
         try:
+            s = Wrapper(c)
             self.run_wrapper(s, paths)
-            sublime.status_message('Done')
         except Exception as e:
             sublime.error_message(str(e))
         finally:
-            s.close()
+            if s:
+                s.close()
 
     def get_path(self, paths=None, directory=False):
         path = None
@@ -62,24 +63,106 @@ class FisPipCommand(sublime_plugin.WindowCommand):
         return True
 
 
-class FisPipSendCommand(FisPipCommand):
+class FisPipFileCommand(FisPipCommand):
+    EXTENSIONS = ['DAT', 'PROC', 'PSL', 'TBL', 'COL']
+
     def run_wrapper(self, wrapper, paths):
         filename = self.get_path(paths)
+        self.run_wrapper_file(wrapper, filename, paths)
+
+    def is_visible(self, paths=None):
+        if not super(FisPipFileCommand, self).is_visible(paths):
+            return False
+
+        path = self.get_path(paths=paths)
+        if os.path.isdir(path):
+            return False
+
+        return os.path.splitext(path)[1][1:].upper() in self.EXTENSIONS
+
+
+class FisPipMissingConfigCommand(FisPipFileCommand):
+    def run(self, paths=None):
+        # does nothing
+        pass
+
+    def is_visible(self, paths=None):
+        if super(FisPipMissingConfigCommand, self).is_visible(paths):
+            return False
+        return True
+
+    def is_enabled(self, paths=None):
+        return False
+
+    def description(self, paths=None):
+        if self.find_config(paths=paths) is None:
+            return 'Missing configuration...'
+        return 'Nothing to do...'
+
+
+class FisPipSendCommand(FisPipFileCommand):
+    def run_wrapper_file(self, wrapper, filename, paths):
         wrapper.send_element(filename)
+        sublime.status_message('%s sent to host' % filename)
 
 
-class FisPipRefreshCommand(FisPipCommand):
-    def run_wrapper(self, wrapper, paths):
-        filename = self.get_path(paths)
+class FisPipGetCommand(FisPipCommand):
+    def run(self, paths=None):
+        self.window.show_input_panel(
+            'Element name (with extension)',
+            '', lambda x: self.run_get(paths, x),
+            None, None
+        )
+
+    def run_get(self, paths, input):
+        c = self.find_config(paths)
+        s = Wrapper(c)
+        try:
+            filename = os.path.join(
+                self.get_path(paths, directory=True),
+                input
+            )
+            fobj = open(filename, 'wb')
+            s.get_element_by_name(filename, file_obj=fobj)
+            sublime.status_message('%s retrieved from host' % filename)
+        except Exception as e:
+            sublime.error_message(str(e))
+        finally:
+            s.close()
+
+
+class FisPipRefreshCommand(FisPipFileCommand):
+    def run_wrapper_file(self, wrapper, filename, paths):
         fobj = open(filename, 'wb')
         wrapper.get_element_by_name(filename, file_obj=fobj)
+        sublime.status_message('%s refreshed' % filename)
 
 
-class FisPipTestCompileCommand(FisPipCommand):
+class FisPipCompileAndLinkCommand(FisPipFileCommand):
+    EXTENSIONS = ['PROC', 'PSL', 'TBL', 'COL']
+
+    def run_wrapper_file(self, wrapper, filename, paths):
+        try:
+            wrapper.compile_and_link(filename)
+            sublime.status_message('%s compiled' % filename)
+        except Exception as e:
+            if e.args[0] != 'ER_SV_INVLDRPC':
+                raise e
+            if sublime.ok_cancel_dialog(
+                '"Compile and Link" requires MRPC081 which seems to be'
+                'disabled in this host.\n'
+                'Do you want to enable it and proceed with compilation?'
+            ):
+                wrapper.enable_mrpc081()
+                wrapper.compile_and_link(filename)
+                sublime.status_message('%s compiled' % filename)
+
+
+class FisPipTestCompileCommand(FisPipFileCommand):
+    EXTENSIONS = ['PROC', 'PSL']
     phantom_sets_by_buffer = {}
 
-    def run_wrapper(self, wrapper, paths):
-        filename = self.get_path(paths)
+    def run_wrapper_file(self, wrapper, filename, paths):
         output = wrapper.test_compile_element(filename).replace('\r', '')
 
         if not hasattr(self, 'output_view'):
@@ -91,7 +174,11 @@ class FisPipTestCompileCommand(FisPipCommand):
              {'characters': output, 'force': True, 'scroll_to_end': True}
         )
 
-        view = sublime.active_window().active_view()
+        sublime.status_message(output.splitlines()[-2])
+        self.add_phantoms(output)
+
+    def add_phantoms(self, output):
+        view = self.window.active_view()
         if not view:
             return
 
@@ -164,10 +251,41 @@ class FisPipTestCompileCommand(FisPipCommand):
         phantom_set.update(phantoms)
 
     def on_phantom_navigate(self, *args):
-        view = sublime.active_window().active_view()
+        view = self.window.active_view()
         if view:
             view.erase_phantoms("fispipstudio")
         self.phantom_sets_by_buffer = {}
+
+
+class FisPipRunPslCommand(FisPipTestCompileCommand):
+    EXTENSIONS = ['PROC', 'PSL']
+
+    def run_wrapper_file(self, wrapper, filename, paths):
+        try:
+            output = wrapper.run_psl(filename)
+        except Exception as e:
+            if e.args[0] != 'ER_SV_INVLDRPC':
+                raise e
+            if not sublime.ok_cancel_dialog(
+                '"Run PSL" requires a custom MRPC to expose PSLRUN^TBXDQSVR.\n'
+                'Do you want to enable it and proceed with operation?'
+            ):
+                return
+            wrapper.enable_mrpc99999()
+            output = wrapper.run_psl(filename)
+        output = output.replace('\r', '')
+
+        if not hasattr(self, 'output_view'):
+            # Try not to call get_output_panel until the regexes are assigned
+            self.output_view = self.window.create_output_panel("fispipstudio")
+        self.window.run_command("show_panel", {"panel": "output.fispipstudio"})
+        self.output_view.run_command(
+             'append',
+             {'characters': output, 'force': True, 'scroll_to_end': True}
+        )
+
+        sublime.status_message(output.splitlines()[-2])
+        self.add_phantoms(output)
 
 
 class FisPipCreateConfigCommand(FisPipCommand):
@@ -180,7 +298,7 @@ class FisPipCreateConfigCommand(FisPipCommand):
             'Packages/FISPIP Studio/fispip-config.json.template'
         )
         open(file, 'w').write(template)
-        sublime.active_window().run_command('open_file', {'file': file})
+        self.window.run_command('open_file', {'file': file})
 
     def is_visible(self, paths=None):
         return not super(FisPipCreateConfigCommand, self).is_visible(paths)
@@ -190,7 +308,7 @@ class FisPipEditConfigCommand(FisPipCommand):
     def run(self, paths=None):
         file = self.find_config(paths=paths)
         if file:
-            sublime.active_window().run_command('open_file', {'file': file})
+            self.window.run_command('open_file', {'file': file})
 
 
 class MRPC121(object):
@@ -256,11 +374,42 @@ class MRPC121(object):
         return r
 
 
+class MRPC081(object):
+    """
+    This procedure exists in PIP but it is not registered as an RPC
+    """
+    def __init__(self, connection=None, mrpc_id='81'):
+        self._con = connection
+        self._id = mrpc_id
+
+    def _call(self, *args):
+        return self._con.executeMRPC(self._id, *args, success_unpack=True)[0]
+
+    def compile(self, table, elements):
+        return self._call(table, elements)
+
+
+class MRPC99999(object):
+    """
+    Custom RPC to expose PSLRUN^TBXDQSVR funcitonality
+    """
+    def __init__(self, connection=None, mrpc_id='99999'):
+        self._con = connection
+        self._id = mrpc_id
+
+    def _call(self, *args):
+        return self._con.executeMRPC(self._id, *args, success_unpack=True)[0]
+
+    def execute(self, compilation_token):
+        return self._call(compilation_token)
+
+
 class Wrapper(PIP):
     OBJ_TYPES = {
         # incomplete mapping based on TBXDQSVR.m
         'DAT': 'Data',
         'PROC': 'Procedure',
+        'PSL': 'Procedure',
         'TBL': 'Table',
         'COL': 'Column'
     }
@@ -269,6 +418,9 @@ class Wrapper(PIP):
         opts = sublime.decode_value(open(conf_file, 'r').read())
         super(Wrapper, self).__init__(opts['server'])
         self._rpc = MRPC121(self)
+        self._rpc81 = MRPC081(self)
+        self._rpc99999 = MRPC99999(self)
+        self._user = opts['user']
         self.connect(
             opts['host'], opts['port'],
             opts['user'], opts['password']
@@ -327,6 +479,67 @@ class Wrapper(PIP):
         else:
             raise Exception('Cannot compile', ext, name)
         return self._rpc81.compile(table, name)
+
+    def run_psl(self, filename, file_obj=None, close_file=True):
+        if file_obj is None:
+            file_obj = open(filename, 'rb')
+            close_file = True
+
+        token = self._send_code(file_obj, close_file)
+        local_file = os.path.basename(filename)
+        return self._rpc99999.execute(token)
+
+    def enable_mrpc081(self):
+        if self.executeSQL(
+            'SELECT RPCID FROM SCATBL5 WHERE RPCID=81'
+        )[0][0] == '81':
+            return
+
+        ucls = self.executeSQL(
+            'SELECT %UCLS FROM SCAU WHERE UID=?',
+            self._user
+        )[0][0]
+        self.executeSQL(
+            "INSERT INTO SCATBL5 "
+            "(%SN,RPCID,MRPC,DESC,LOGFLG,PARAM01,PARAM02) VALUES"
+            "('PBS','81','$$^MRPC081','Compile DQ Runtime Routines',1,1,1)"
+        )
+        # TODO: pyfispip not supporting markers on INSERT
+        # update this once it does
+        self.executeSQL(
+            "INSERT INTO SCATBL5A (RPCID,UCLS,AUTH,LOGFLG) "
+            "VALUES('81','%s',0,0)" % ucls
+        )
+
+    def enable_mrpc99999(self):
+        if self.executeSQL(
+            'SELECT RPCID FROM SCATBL5 WHERE RPCID=99999'
+        )[0][0] == '99999':
+            return
+
+        self.enable_mrpc081()
+        template = sublime.load_resource(
+            'Packages/FISPIP Studio/MRPC99999.PROC'
+        )
+        fobj = StringIO(template)
+        self.send_element('MRPC99999.PROC', file_obj=fobj)
+        self.compile_and_link('MRPC99999.PROC')
+
+        ucls = self.executeSQL(
+            'SELECT %UCLS FROM SCAU WHERE UID=?',
+            self._user
+        )[0][0]
+        self.executeSQL(
+            "INSERT INTO SCATBL5 "
+            "(%SN,RPCID,MRPC,DESC,LOGFLG,PARAM01) VALUES"
+            "('PBS','99999','$$^MRPC99999','Run PSL Remotely',1,1)"
+        )
+        # TODO: pyfispip not supporting markers on INSERT
+        # update this once it does
+        self.executeSQL(
+            "INSERT INTO SCATBL5A (RPCID,UCLS,AUTH,LOGFLG) "
+            "VALUES('99999','%s',0,0)" % ucls
+        )
 
     def _send_code(self, file_obj, close_file=True):
         token = ''
